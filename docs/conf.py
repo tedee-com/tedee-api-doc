d629f432dad524d52459379acb25fdaaff6e2079
Konrad Sikorski <konrad.sikorski@windowslive,cin>
2020-03-26 10:46:22 +0100

    fixed project variable again

--- docs/conf.py
+++ docs/conf.py
@@ -18,7 +18,7 @@ import sphinx_rtd_theme
 
 # -- Project information -----------------------------------------------------
 
-project = 'Tedee api docs'
+project = 'Tedee API documentation'
 copyright = '2020, Tedee'
 author = 'Tedee'
 clientId = '02106b82-0524-4fd3-ac57-af774f340979'
