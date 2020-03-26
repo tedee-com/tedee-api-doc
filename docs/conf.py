e72a3d61327843cecefdbe16b63ece022908169e
Adam Żurek <adam.zurek@predicagroup.com>
2020-03-26 08:27:35 +0100

    Move copybutton extension to other PR.

--- docs/conf.py
+++ docs/conf.py
@@ -33,7 +33,6 @@ authApiUrl = 'https://tedee.b2clogin.com/tedee.onmicrosoft.com'
 # ones.
 extensions = [
     "sphinx_rtd_theme",
-    "sphinx_copybutton"
 ]
 
 # Add any paths that contain templates here, relative to this directory.
